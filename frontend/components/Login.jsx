adf4321d9f580a179d6ee0d417c6376685491495
ConorRoberts1 <G00406299@atu.ie>
2025-01-21 13:51:41 +0000

    Added animatable text

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -1,53 +1,73 @@
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native'
-import React from 'react'
-import { Colors } from '@/constants/Colors'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { Colors } from '@/constants/Colors'; 
 import { useRouter } from 'expo-router';
-
-
+import { BackgroundGradient } from '../constants/globalStyles'; 
+import * as Animatable from 'react-native-animatable';
 
 export default function Login() {
+  const router = useRouter();
 
-  const router=useRouter();
   return (
-    <View style={styles.container}>
-    <Text style={{
-      fontSize: 28,
-      fontWeight: 'bold',
-      textAlign: 'center'
-      
-    }}>Roam Routes</Text>
+    <BackgroundGradient>
+      <View style={styles.container}>
+        <Animatable.Text
+          animation="fadeIn"
+          duration={1000}
+          style={styles.title}
+          delay={0}
+        >
+          Roam Routes
+        </Animatable.Text>
 
-    <Text style={{
-      fontSize: 16,
-      textAlign: 'center',
-      color: Colors.grey,
-      marginTop: 10
-    }}>Discover new places and meet new people!</Text>
+        <Animatable.Text
+          animation="fadeIn"
+          duration={1000}
+          style={styles.subtitle}
+          delay={500}
+        >
+          Discover new places and meet new people!
+        </Animatable.Text>
 
-    <TouchableOpacity style={styles.button}
-    onPress={()=>router.push('/auth/sign-in')}>
-      <Text style={{color: Colors.WHITE, 
-      textAlign:'center',
-      fontSize: 20}}>Get Started!</Text>
-    </TouchableOpacity>
-  </View>
-  )
-} 
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => router.push('/auth/sign-in')}
+        >
+          <Text style={styles.buttonText}>Get Started!</Text>
+        </TouchableOpacity>
+      </View>
+    </BackgroundGradient>
+  );
+}
 
 const styles = StyleSheet.create({
-  container:{
-      backgroundColor:'white',
-      marginTop: 60,
-      height: '100%',
-      borderTopLeftRadius: 20,
-      borderTopRightRadius: 20,
-      padding: 50
+  container: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    padding: 25,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: Colors.grey,
+    marginTop: 20,
   },
   button: {
-    padding: 15 ,
-    backgroundColor:'black',
+    padding: 15,
+    backgroundColor: 'black',
     borderRadius: 99,
-    marginTop: '25%'
-
-  }
-})
+    marginTop: 60,
+    width: '80%', 
+  },
+  buttonText: {
+    color: Colors.WHITE,
+    textAlign: 'center',
+    fontSize: 20,
+  },
+});
