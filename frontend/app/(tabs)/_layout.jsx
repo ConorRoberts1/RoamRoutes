63675e045c8a8f4b3c118ea08fa9f4451cbdaceb
ConorRoberts1 <G00406299@atu.ie>
2025-04-27 06:23:07 +0100

    changed function name

--- frontend/app/(tabs)/_layout.jsx
+++ frontend/app/(tabs)/_layout.jsx
@@ -2,56 +2,51 @@ import React from 'react';
 import { Tabs } from 'expo-router';
 import { FontAwesome, FontAwesome5, Feather, Fontisto } from '@expo/vector-icons';
 
-
-
-const TabLayout = () => {
-    return (
-        <Tabs
-            screenOptions={{
-                headerShown: false,
-                tabBarActiveTintColor: 'black',
-                tabBarInactiveTintColor: 'gray',
-            }}
-        >
-            <Tabs.Screen
-                name="trips"
-                options={{
-                    tabBarLabel: 'My Trips',
-                    tabBarIcon: ({ color }) => (
-                        <Fontisto name="holiday-village" size={24} color={color} />
-                    ),
-                }}
-            />
-            <Tabs.Screen
-                name="connections"
-              
-                options={{
-                    tabBarLabel: "Connections",
-                    tabBarIcon: ({ color }) => (
-                        <FontAwesome5 name="handshake" size={24} color={color} />
-                    ),
-                }}
-            />
-            <Tabs.Screen 
-                name="messages"
-                options={{
-                    tabBarLabel: 'Messages',
-                    tabBarIcon: ({ color }) => (
-                        <Feather name="message-square" size={24} color={color} />
-                    ),
-                }}/>
-            
-            <Tabs.Screen
-                name="profile"
-                options={{
-                    tabBarLabel: 'Profile',
-                    tabBarIcon: ({ color }) => (
-                        <FontAwesome name="user-circle" size={24} color={color} />
-                    ),
-                }}
-            />
-        </Tabs>
-    );
-};
-
-export default TabLayout;
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'gray',
+      }}
+    >
+      <Tabs.Screen
+        name="trips"
+        options={{
+          tabBarLabel: 'My Trips',
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="holiday-village" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="connections"
+        options={{
+          tabBarLabel: "Connections",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="handshake" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="messages"
+        options={{
+          tabBarLabel: 'Messages',
+          tabBarIcon: ({ color }) => (
+            <Feather name="message-square" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user-circle" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
